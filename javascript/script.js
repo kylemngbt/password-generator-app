59ac893d4890f9479ffa03e939c64df459f82579
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-04 19:10:34 +0800

    build: create allChars variable

--- javascript/script.js
+++ javascript/script.js
@@ -4,4 +4,4 @@ const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "@#$%^&*()_+~|}{[]></-=";
-
+const allChars = upperCase + lowerCase + number + symbol;
